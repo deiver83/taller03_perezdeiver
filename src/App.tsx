1e79fd73526fd5cc2365f7a92e60475fc011a9d1
deiver <dperez83@cuc.edu.co>
2024-09-24 13:01:31 -0500

    m1

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+import miLogo from "../src/assets/img/images..jpeg"
+
+function App() {
+
+
+  return (
+    <>
+      <div>
+        <img src={miLogo} alt="Ronaldo" />
+        <h1>taller03</h1>
+      </div>
+    </>
+  )
+}
+
+export default App
